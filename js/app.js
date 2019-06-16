b1ed8a1c2be8fb0647fa1bd603d4fd8c685bfdcd
Valerie Sobol <vssobol@gmail.com>
2019-06-15 17:49:44 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -28,32 +28,49 @@ var CartItem = function(product, quantity) {
 var Product = function(filePath, name) {
   this.filePath = filePath;
   this.name = name;
-  Product.allProducts.push(this);
+  allProducts.push(this);
 };
-Product.allProducts = [];
+var allProducts = [];
 
 function generateCatalog() {
-  new Product('assets/bag.jpg', 'Bag');
-  new Product('assets/banana.jpg', 'Banana');
-  new Product('assets/bathroom.jpg', 'Bathroom');
-  new Product('assets/boots.jpg', 'Boots');
-  new Product('assets/breakfast.jpg', 'Breakfast');
-  new Product('assets/bubblegum.jpg', 'Bubblegum');
-  new Product('assets/chair.jpg', 'Chair');
-  new Product('assets/cthulhu.jpg', 'Cthulhu');
-  new Product('assets/dog-duck.jpg', 'Dog-Duck');
-  new Product('assets/dragon.jpg', 'Dragon');
-  new Product('assets/pen.jpg', 'Pen');
-  new Product('assets/pet-sweep.jpg', 'Pet Sweep');
-  new Product('assets/scissors.jpg', 'Scissors');
-  new Product('assets/shark.jpg', 'Shark');
-  new Product('assets/sweep.png', 'Sweep');
-  new Product('assets/tauntaun.jpg', 'Taun-Taun');
-  new Product('assets/unicorn.jpg', 'Unicorn');
-  new Product('assets/usb.gif', 'USB');
-  new Product('assets/water-can.jpg', 'Water Can');
-  new Product('assets/wine-glass.jpg', 'Wine Glass');
+  new Product('img/bag.jpg', 'Bag');
+  new Product('img/banana.jpg', 'Banana');
+  new Product('img/bathroom.jpg', 'Bathroom');
+  new Product('img/boots.jpg', 'Boots');
+  new Product('img/breakfast.jpg', 'Breakfast');
+  new Product('img/bubblegum.jpg', 'Bubblegum');
+  new Product('img/chair.jpg', 'Chair');
+  new Product('img/cthulhu.jpg', 'Cthulhu');
+  new Product('img/dog-duck.jpg', 'Dog-Duck');
+  new Product('img/dragon.jpg', 'Dragon');
+  new Product('img/pen.jpg', 'Pen');
+  new Product('img/pet-sweep.jpg', 'Pet Sweep');
+  new Product('img/scissors.jpg', 'Scissors');
+  new Product('img/shark.jpg', 'Shark');
+  new Product('img/sweep.png', 'Sweep');
+  new Product('img/tauntaun.jpg', 'Taun-Taun');
+  new Product('img/unicorn.jpg', 'Unicorn');
+  new Product('img/usb.gif', 'USB');
+  new Product('img/water-can.jpg', 'Water Can');
+  new Product('img/wine-glass.jpg', 'Wine Glass');
 }
 
 // Initialize the app by creating the big list of products with images and names
 generateCatalog();
+
+
+/* Create new element with images and append to col-2 */
+
+var sectionEl = document.getElementsByClassName('col-1')[0];
+var divEl = document.createElement('div');
+divEl.setAttribute('id', "imageContainer");
+for(var i = 0; i < allProducts.length; i++){
+  var imgEl = document.createElement('img');
+  imgEl.setAttribute('src', allProducts[i].filePath);
+  imgEl.setAttribute('alt', allProducts[i].name);
+  imgEl.setAttribute('title', allProducts[i].name);
+  imgEl.setAttribute('height', 150);
+  imgEl.setAttribute('width', 150);
+  divEl.appendChild(imgEl);
+}
+sectionEl.appendChild(divEl);
